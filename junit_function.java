d0094bbcc3771e601e48135588ff83ad0cfbf0c5
Abhinav Robinson <abhinavrobinson@gmail.com>
2020-05-10 20:28:14 +0530

    Lab 9 Upload

---
+++ junit_function.java
@@ -0,0 +1,11 @@
+package junit_testing_package;
+
+public class junit_function {
+	public int add_num(int num1,int num2) {
+		return num1+num2;
+	}
+	
+	public String addstring(String str1,String str2) {
+		return str1+str2;
+	}
+}
